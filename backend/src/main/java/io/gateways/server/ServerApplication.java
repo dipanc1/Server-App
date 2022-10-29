704aff08ce410c027befc66c06cefc8643245c8c
dipanc1 <dipanchhabra@gmail.com>
2022-10-29 09:46:47 +0530

    ping server and added beans

--- backend/src/main/java/io/gateways/server/ServerApplication.java
+++ backend/src/main/java/io/gateways/server/ServerApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -23,5 +28,18 @@ public class ServerApplication {
 			serverRepo.save(new Server(null, "192.168.1.21", "Jatt da LAPTOP", "32 GB", "John Type", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.0.1", "Jatt da Sher", "64 GB", "Jatti Type", "http://localhost:8080/server/image/server4.png", Status.SERVER_DOWN));
 		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter(){
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin","Content-Type","Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type","Accept", "Jwt-Token", "Authorization", "Access-Control-Request-Origin","Access-Control-Allow-Origin", "Filename","Access-Control-Request-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, startWith } from 'rxjs';
+import { map } from 'rxjs';
+import { catchError } from 'rxjs';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+import { Status } from './enum/status.enum';
+
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-reponse';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>
+
+  constructor(private serverService: ServerService) { }
+
+  readonly DataState = DataState;
+  readonly Status = Status;
+
+  private filterSubject = new BehaviorSubject<string>('');
+  private dataSubject = new BehaviorSubject<CustomResponse>(null);
+  filterStatus$ = this.filterSubject.asObservable();
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => {
+          return {
+            dataState: DataState.LOADED_STATE, appData: response
+          }
+        }),
+        startWith({
+          dataState: DataState.LOADING_STATE
+        }),
+        catchError((error: string) => {
+          return of({
+            dataState: DataState.ERROR_STATE,
+            error
+          })
+        })
+      )
+  }
+
+  pingServer(ipAddress: string): void {
+    this.filterSubject.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+      .pipe(
+        map(response => {
+          this.dataSubject.next()
+          return {
+            dataState: DataState.LOADED_STATE, appData: response
+          }
+        }),
+        startWith({
+          dataState: DataState.LOADING_STATE
+        }),
+        catchError((error: string) => {
+          return of({
+            dataState: DataState.ERROR_STATE,
+            error
+          })
+        })
+      )
+  }
+
 }
